4c4255bb8ecb1b398fcf758a0df87ed5f01d8e66
eyujunior <eyujunior052@gmail.com>
2020-11-18 13:52:09 -0800

    added testimonial section using glider.js for sliding

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ window.addEventListener("scroll", () => {
     headerContainer.classList.remove('light');
   }
 });
+
+new Glider(document.querySelector('.glider'), {
+  slidesToShow: 2,
+  draggable: true,
+  dots: '#dots',
+  arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+  }
+});
