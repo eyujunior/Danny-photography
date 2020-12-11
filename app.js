37ed863f8cee7338e7f6d8d8402a53c02221d834
eyujunior <eyujunior052@gmail.com>
2020-12-11 13:52:08 -0800

    mobile and desktop version completed

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-const headerContainer = document.querySelector(".header-container");
+const header = document.querySelector(".header");
+const bars = document.querySelector('.bars');
+
+const exit = document.getElementById('exit');
+// close mobile navigation
+exit.addEventListener('click', () => header.classList.remove('mobile'))
+// hamburger menu
+bars.addEventListener('click', () => header.classList.toggle('mobile'));
+
+document.querySelector('main').addEventListener('click', () => {
+  if(header.classList.contains('mobile'))
+  {
+    header.classList.remove('mobile');
+  }
+})
+
+
+// navigation background changer
 window.addEventListener("scroll", () => {
   if (window.pageYOffset >= 20) {
-    headerContainer.classList.add('light');
+    header.classList.add('light');
   } else {
-    headerContainer.classList.remove('light');
+    header.classList.remove('light');
   }
 });
 
-new Glider(document.querySelector('.glider'), {
-  slidesToShow: 2,
-  draggable: true,
-  dots: '#dots',
-  arrows: {
-      prev: '.glider-prev',
-      next: '.glider-next'
-  }
+// new Glider(document.querySelector('.glider'), {
+//   slidesToShow: 2,
+//   draggable: true,
+//   dots: '#dots',
+//   arrows: {
+//       prev: '.glider-prev',
+//       next: '.glider-next'
+//   }
+// });
+
+document.querySelectorAll('.btn').forEach(element => {
+  element.addEventListener('click', () => {
+    console.log('clicked');
+  })
 });
