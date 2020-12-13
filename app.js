a1a0cbdc8e087d3e5de856ea51e177fcb87bc3cc
eyujunior <eyujunior052@gmail.com>
2020-12-13 15:54:02 -0800

    basic animations added

--- app.js
+++ app.js
@@ -1,41 +1,69 @@
 const header = document.querySelector(".header");
-const bars = document.querySelector('.bars');
+const bars = document.querySelector(".bars");
 
-const exit = document.getElementById('exit');
+const exit = document.getElementById("exit");
 // close mobile navigation
-exit.addEventListener('click', () => header.classList.remove('mobile'))
+exit.addEventListener("click", () => header.classList.remove("mobile"));
 // hamburger menu
-bars.addEventListener('click', () => header.classList.toggle('mobile'));
+bars.addEventListener("click", () => header.classList.toggle("mobile"));
 
-document.querySelector('main').addEventListener('click', () => {
-  if(header.classList.contains('mobile'))
-  {
-    header.classList.remove('mobile');
+document.querySelector("main").addEventListener("click", () => {
+  if (header.classList.contains("mobile")) {
+    header.classList.remove("mobile");
   }
-})
-
+});
 
 // navigation background changer
 window.addEventListener("scroll", () => {
   if (window.pageYOffset >= 20) {
-    header.classList.add('light');
+    header.classList.add("light");
   } else {
-    header.classList.remove('light');
+    header.classList.remove("light");
   }
 });
 
-// new Glider(document.querySelector('.glider'), {
-//   slidesToShow: 2,
-//   draggable: true,
-//   dots: '#dots',
-//   arrows: {
-//       prev: '.glider-prev',
-//       next: '.glider-next'
-//   }
-// });
+new Glider(document.querySelector(".glider"), {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  scrollLock: true,
+  draggable: false,
+  dots: "#resp-dots",
+  arrows: {
+    prev: ".glider-prev",
+    next: ".glider-next",
+  },
 
-document.querySelectorAll('.btn').forEach(element => {
-  element.addEventListener('click', () => {
-    console.log('clicked');
-  })
+  responsive: [
+    {
+      breakpoint: 775,
+      settings: {
+        slidesToShow: "auto",
+        slidesToScroll: "auto",
+        itemWidth: 250,
+        duration: 0.25,
+      },
+    },
+    {
+      breakpoint:1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        itemWidth: 250,
+        duration: 0.25,
+      }
+    },
+  ],
 });
+
+document.querySelectorAll(".btn").forEach((element) => {
+  element.addEventListener("click", () => {
+    console.log("clicked");
+  });
+});
+
+
+//gsap animations
+
+gsap.from('.col-2', {opacity:0, height:10, duration: 2});
+gsap.from('.col-1', {opacity:0, y:200, duration: 2});
+gsap.from('.header', {opacity:0, duration: 2, delay: 1});
